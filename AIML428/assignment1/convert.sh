634d70e32acc60332406196c162566772f411d1d
Jason Pollock <jason@pollock.ca>
2026-03-25 19:47:01 +1300

    Step 5: Complete
    Ignore the examples, they didn't parse the Glove file properly.
    Lowercase the words - Glove is all lowercase.

--- AIML428/assignment1/convert.sh
+++ AIML428/assignment1/convert.sh
@@ -5,5 +5,5 @@ in_file=$1
 target="$(basename "$(dirname "$in_file")")"
 
 printf "${target},"
-cat $in_file | tr '[:punct:]' ' ' | tr '\n\r\t\v\f' " " | tr -s "[:space:]" | sed 's/^  *//'
+cat $in_file | tr '[:upper:]' '[:lower:]' | tr '[:punct:]' ' ' | tr '\n\r\t\v\f' " " | tr -s "[:space:]" | sed 's/^  *//'
 printf "\n"
