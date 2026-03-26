5d5c972202a2efe8c6701abbacb1af49ae8671b3
Jason Pollock <jason@pollock.ca>
2026-03-27 07:39:13 +1300

    Step 4,5 complete.
    Step6 - misbehaving.

--- AIML428/assignment1/convert_all.sh
+++ AIML428/assignment1/convert_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 out_file=bbc_converted.csv
+cp /dev/null $out_file
 printf "category,text\n" > $out_file
-for i in `find . -name *.txt -print` ; do
+for i in `find data/bbc* -name *.txt -print` ; do
     echo "Converting $i"
     ./convert.sh $i >>$out_file
 done
